ce6d3a924471f8911044df8183efe7926668f234
Cak Yan <akhdanifebryan@gmail.com>
2023-03-26 14:45:42 +0700

    perbaikan minor

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,10 +53,17 @@
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <a href="index.html" class="">
+                            <a href="" class="">
                                 <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>SIK -  Rawat Inap</h3>
                             </a>
                         </div>
+
+                        @if (session()->has('Login Gagal'))
+                            <div class="alert alert-danger alert-dismissable fade show" role="alert">
+                            {{ session('Login Gagal') }}
+                            </div>
+                        @endif
+
                         <h3>Sign In</h3>
                         <form action="login_validation" method="POST">
                             @csrf
